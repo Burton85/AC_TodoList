696e2ec4a6c74bcdef76bb2d8796f5004360b74b
Burton85 <burton00tw@gmail.com>
2019-12-13 22:33:17 +0800

    Add auth middleware and todo,home routers

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const port = "3000";
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
+const session = require("express-session");
+const passport = require("passport");
+app.use(
+  session({
+    secret: "your secret key",
+    resave: "false",
+    saveUninitialized: "false"
+  })
+);
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -17,8 +26,20 @@ const User = db.User;
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+
+// 使用 Passport - 要在「使用路由器」前面
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./config/passport")(passport);
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
 // 使用路由器
+app.use("/", require("./routes/home"));
 app.use("/users", require("./routes/user"));
+app.use("/todos", require("./routes/todo"));
 // 設定 express port 3000 與資料庫同步
 
 app.listen(port, () => {
